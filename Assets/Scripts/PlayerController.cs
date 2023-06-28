24eee7329cfd2552040ed0bd24cd5f98672da596
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-06-28 09:10:44 +0300

    toka päivä

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // komponentit
     private Rigidbody2D body;
+    private Animator animator;
     // input-muuttujat
     private float horizontalMovement;
     // liikkumismuuttujat
@@ -12,10 +14,12 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement = new Vector2();
     // hyppimismuuttujat
     private float jumpForce = 10f;
+    private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,13 +27,28 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         movement.x = horizontalMovement * moveSpeed;
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+        animator.SetFloat("speed", Mathf.Abs(horizontalMovement));
     }
     void FixedUpdate()
     {
         transform.Translate(movement * Time.deltaTime);
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            grounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            grounded = false;
+        }
     }
 }
